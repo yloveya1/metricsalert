3e49255e71d300e2d70f4a6a24b234f2a4f22a39
toddredw <ylovyagina00@gmail.com>
2026-03-28 22:01:24 +0300

    test

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,13 +2,16 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/yloveya1/metricsalert/internal/handler"
 )
 
 func New(h *handler.Handler) chi.Router {
 	r := chi.NewRouter()
 
+	r.Use(middleware.Recoverer)
 	r.Use(h.WithLogging())
+	r.Use(middleware.RedirectSlashes)
 
 	r.Route("/update/", func(r chi.Router) {
 		r.Post("/", h.UpdateMetricFromBody)
