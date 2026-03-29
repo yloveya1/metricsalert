57e0aa45578a4389a250984248d144b11b9dad23
toddredw <ylovyagina00@gmail.com>
2026-03-29 14:49:20 +0300

    add recoverer

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,8 +9,8 @@ import (
 func New(h *handler.Handler) chi.Router {
 	r := chi.NewRouter()
 
-	r.Use(middleware.Recoverer)
 	r.Use(h.WithLogging())
+	r.Use(middleware.Recoverer)
 
 	r.Get("/", h.GetMetricList)
 
