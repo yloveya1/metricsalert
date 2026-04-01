9d5f58fa80700ab8ee6d25fbbaaa4bb8c093fed9
toddredw <ylovyagina00@gmail.com>
2026-04-01 22:54:03 +0300

    add header

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -83,6 +83,7 @@ func GzipMiddleware(h http.Handler) http.Handler {
 		if supportsGzip {
 			cw := newCompressWriter(w)
 			ow = cw
+			ow.Header().Add("Content-Encoding", "gzip")
 			defer cw.Close()
 		}
 
