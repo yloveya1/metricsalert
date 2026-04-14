68cde55826d03b8a2b88128d4dfbcf5e8201a32b
toddredw <ylovyagina00@gmail.com>
2026-04-14 23:24:23 +0300

    delete double import

--- internal/repository/pg/pg.go
+++ internal/repository/pg/pg.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 	models "github.com/yloveya1/metricsalert/internal/model"
