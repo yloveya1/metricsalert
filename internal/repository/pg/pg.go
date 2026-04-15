41cca8886fc646aede916e2c5aaea2924d2a4b9a
toddredw <ylovyagina00@gmail.com>
2026-04-16 02:13:26 +0300

    add log

--- internal/repository/pg/pg.go
+++ internal/repository/pg/pg.go
@@ -21,7 +21,7 @@ const (
         VALUES ($1, $2, $3, $4)
         ON CONFLICT (name) DO UPDATE SET
             type  = EXCLUDED.type,
-            delta = COALESCE(metrics.delta, 0) + EXCLUDED.delta,
+            delta = metrics.delta + EXCLUDED.delta,
             value = EXCLUDED.value,
             updated_at = NOW();`
 )
