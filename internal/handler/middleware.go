de8d344f41e975137a13b5e732c067a9e9bdb245
toddredw <ylovyagina00@gmail.com>
2026-05-11 10:00:07 +0300

    test

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -138,41 +138,64 @@ func (h *Handler) WithLogging() func(http.Handler) http.Handler {
 
 func (h *Handler) HashMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if len(r.Header.Get(contentHeader)) == 0 || len(h.key) == 0 {
+		if len(h.key) == 0 {
 			next.ServeHTTP(w, r)
 			return
+		}
+
+		rec := &hashResponseWriter{
+			ResponseWriter: w,
+			key:            []byte(h.key),
 		}
 
 		hashHeaderValue := r.Header.Get(hashHeader)
-		if hashHeaderValue == "" {
-			next.ServeHTTP(w, r)
-			return
+		if hashHeaderValue != "" {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+				return
+			}
+			r.Body.Close()
+			r.Body = io.NopCloser(bytes.NewReader(body))
+
+			hasher := hmac.New(sha256.New, []byte(h.key))
+			hasher.Write(body)
+			computedMAC := hasher.Sum(nil)
+
+			decodedMAC, err := hex.DecodeString(hashHeaderValue)
+			if err != nil {
+				http.Error(w, "Invalid hash encoding", http.StatusBadRequest)
+				return
+			}
+
+			if !hmac.Equal(computedMAC, decodedMAC) {
+				http.Error(w, "Invalid hash", http.StatusBadRequest)
+				return
+			}
 		}
 
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-			return
+		next.ServeHTTP(rec, r)
+
+		if len(rec.body.Bytes()) > 0 {
+			hasher := hmac.New(sha256.New, []byte(h.key))
+			hasher.Write(rec.body.Bytes())
+			rec.Header().Set(hashHeader, hex.EncodeToString(hasher.Sum(nil)))
 		}
-
-		r.Body.Close()
-		r.Body = io.NopCloser(bytes.NewReader(body))
-
-		hasher := hmac.New(sha256.New, []byte(h.key))
-		hasher.Write(body)
-		computedMAC := hasher.Sum(nil)
-
-		decodedMAC, err := hex.DecodeString(hashHeaderValue)
-		if err != nil {
-			http.Error(w, "Invalid hash encoding", http.StatusBadRequest)
-			return
-		}
-
-		if !hmac.Equal(computedMAC, decodedMAC) {
-			http.Error(w, "Invalid hash", http.StatusBadRequest)
-			return
-		}
-
-		next.ServeHTTP(w, r)
 	})
+}
+
+// hashResponseWriter обертка для ResponseWriter, которая сохраняет тело ответа
+type hashResponseWriter struct {
+	http.ResponseWriter
+	body bytes.Buffer
+	key  []byte
+}
+
+func (h *hashResponseWriter) Write(b []byte) (int, error) {
+	h.body.Write(b)
+	return h.ResponseWriter.Write(b)
+}
+
+func (h *hashResponseWriter) WriteHeader(statusCode int) {
+	h.ResponseWriter.WriteHeader(statusCode)
 }
